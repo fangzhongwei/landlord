3a27239c1a05c7eb455180eb208461dec480cb3e
fangzhongwei <zhongwei.fang@56qq.com>
2017-02-28 14:15:20 +0800

    touch

--- Assets/Scripts/App/UI/CardAttr.cs
+++ Assets/Scripts/App/UI/CardAttr.cs
@@ -3,25 +3,6 @@
 public class CardAttr : MonoBehaviour
 {
     public bool ready2go { get; set; }
+    public bool inHand { get; set; }
     public int idx { get; set; }
-
-    void Start()
-    {
-        if ("card1".Equals(gameObject.name))
-        {
-            ready2go = false;
-            idx = 0;
-        }
-        else if ("card2".Equals(gameObject.name))
-        {
-            ready2go = false;
-            idx = 1;
-        }
-        else if ("card3".Equals(gameObject.name))
-        {
-            ready2go = false;
-            idx = 2;
-        }
-    }
-
 }
--- Assets/Scripts/App/UI/TweenCardMove.cs
+++ Assets/Scripts/App/UI/TweenCardMove.cs
@@ -1,17 +1,11 @@
 ﻿
 using App.Base;
-using UnityEngine;
 
 public class TweenCardMove : UITweener
 {
     protected override void OnUpdate(float factor, bool isFinished)
     {
-        Debug.Log(string.Format("OnUpdate called, factor:{0}, isFinished:{1}", factor, isFinished));
-//        Vector3 from = transform.position;
-//        Vector3 to = from + Vector3.right;
-//        Vector3 value = from * (1f - factor) + to * factor;
-//        transform.localPosition += Vector3.up * 100;
-        if (TouchManager.GetInstance().touching)
+        if (GetComponent<CardAttr>().inHand && TouchManager.GetInstance().touching)
         {
             OnFocus();
         }
